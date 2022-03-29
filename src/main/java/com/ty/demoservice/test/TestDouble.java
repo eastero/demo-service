a32a7ca0222e8acea2903e8433d6147cc28477d8
eastero <easteros@foxmail.com>
2022-03-29 19:08:50 +0800

    add double test

---
+++ src/main/java/com/ty/demoservice/test/TestDouble.java
@@ -0,0 +1,11 @@
+package com.ty.demoservice.test;
+
+import java.util.Random;
+
+public class TestDouble {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r);
+    }
+}
